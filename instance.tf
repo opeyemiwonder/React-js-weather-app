b6e754966ea87f849865a41399a3d5cad3ea191d
opeyemiwonder <adeyemiopeyemi190@yahoo.com>
2023-02-16 13:32:23 +0100

    updated terraform file

--- instance.tf
+++ instance.tf
@@ -1,21 +1,24 @@
-resource "aws_instance" "terraform-server" { 
-  ami           = "ami-0036b4598ccd42565" 
-  instance_type = "t2.micro" 
+resource "aws_instance" "terraform-server" {
+  ami               = "ami-0d50e5e845c552faf"
+  instance_type     = "t2.micro"
   availability_zone = "us-west-1b"
-  key_name = "Hng_key"
-  
-  network_interface {
-    device_index = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
+  key_name          = "Hng_key"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update
-              sudo apt install nginx
-              sudo systemctl start nginx.service
-              EOF
-  tags = { 
-    Name = "dev-server" 
-  } 
+  
+#   network_interface {
+#     device_index         = 0
+#     network_interface_id = aws_network_interface.web-server-nic.id
+#   }
+
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               sudo apt update
+#               sudo apt install nginx
+#               sudo systemctl start nginx.service
+#               EOF
+      
+  tags = {
+    Name = "dev-server"
+  }
 }
